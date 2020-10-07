ab40c3fb18ddc834ca82b4bc21972e6ef8d1ea59
in2dam-b <in2dam-b@HZ061524.elorrieta.int>
2020-10-07 20:15:38 +0200

    utilidades

---
+++ src/Modelo/Utilidades.java
@@ -0,0 +1,18 @@
+package Modelo;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Utilidades {
+
+	public static boolean validar(String pat, String texto) {
+		boolean validado = false;
+		Pattern patron = Pattern.compile(pat);
+		Matcher mat = patron.matcher(texto);
+		if (mat.matches()) {
+			validado = true;
+		}
+		return validado;
+	}
+
+}
