812aa266b356efe8186684d87b3f8406592e294b
in2dam-b <in2dam-b@HZ061524.elorrieta.int>
2020-10-08 18:47:12 +0200

    crear fichero sin acabar

--- src/Modelo/ficheroCsv.java
+++ src/Modelo/ficheroCsv.java
@@ -27,6 +27,7 @@ public class ficheroCsv {
 	         
 	      } catch (Exception e) {
 	         e.printStackTrace();
+	         System.out.println("Error al leer fichero");
 	      } finally {
 	         if (null!=br) {
 	            try {
@@ -38,6 +39,38 @@ public class ficheroCsv {
 	         }
 	}
 	}
-	     
+	/*public static void main(String[] args) {
+		final String nombreDeArchivo = "./files/archivo.csv";
+		crearArchivoCSV(nombreDeArchivo);
+	}
+
+	private static void crearArchivoCSV(String nombreDeArchivo) {
+		// this also can be "\t" for tab delimitador
+		crearArchivoCSV(nombreDeArchivo, ",");
+	}
+
+	private static void crearArchivoCSV(String file, String delim) {
+		final String NEXT_LINE = "\n";
+		try {
+			FileWriter fw = new FileWriter(file);
+
+			fw.append("testing").append(delim)
+			fw.append("123").append(NEXT_LINE);
+
+			fw.append("value1");
+			fw.append(delim);
+			fw.append("312");
+			fw.append(NEXT_LINE);
+
+			fw.append("anotherthing,888\n");
+
+			fw.flush();
+			fw.close();
+		} catch (IOException e) {
+			// Error al crear el archivo, por ejemplo, el archivo 
+			// está actualmente abierto.
+			e.printStackTrace();
+		}
+	}*/
 	}
 
